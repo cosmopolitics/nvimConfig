7d6fdf33f9e78dd9d3407975b2632df4034b77e0
Cosmo Waterbury <atticuswaterbury@gmail.com>
2024-12-07 20:17:12 -0800

    init of new

--- lua/cosmo/lazyplugs/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'nvim-lua/plenary.nvim',
-    'mbbill/undotree',
-    'tpope/vim-fugitive',
-    'nvim-treesitter/nvim-treesitter-context',
-    'eandrju/cellular-automaton.nvim',
-    'laytan/cloak.nvim',
-    'j-hui/fidget.nvim',
-}
