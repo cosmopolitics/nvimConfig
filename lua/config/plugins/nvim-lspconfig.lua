0c7f8d4098079cb874c9f5a3aa51074a8775209a
Cosmo Waterbury <atticuswaterbury@gmail.com>
2024-12-23 19:18:45 -0800

    idk

--- lua/config/plugins/nvim-lspconfig.lua
+++ lua/config/plugins/nvim-lspconfig.lua
@@ -2,22 +2,20 @@ return {
   {
     'neovim/nvim-lspconfig',
     dependencies = {
+      'saghen/blink.cmp',
       {
         "folke/lazydev.nvim",
         ft = "lua", -- only load on lua files
         opts = {
           library = {
-            -- See the configuration section for more details
-            -- Load luvit types when the `vim.uv` word is found
             { path = "${3rd}/luv/library", words = { "vim%.uv" } },
           },
         },
       },
     },
     config = function()
-      require("lspconfig").lua_ls.setup {}
-      require("lspconfig").pyright.setup {}
-      require("lspconfig").nil_ls.setup {}
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      require("lspconfig").lua_ls.setup { capabilities = capabilities }
 
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
@@ -27,7 +25,6 @@ return {
 
           if client.supports_method('textDocument/formatting') then
             vim.api.nvim_create_autocmd('BufWritePre', {
-
               buffer = args.buf,
               callback = function()
                 vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
