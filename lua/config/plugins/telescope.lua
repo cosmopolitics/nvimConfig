91c487646a911527b1369fc402c5594e4aedf1c5
Cosmo Waterbury <cosmowaterbury@gmail.com>
2025-07-27 03:49:15 -0700

    lsp telescope and undo

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -8,15 +8,18 @@ return {
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     },
     config = function()
-      require('telescope').setup {
-        extensions = {
-          fzf = {}
-        }
-      }
-      vim.keymap.set("n", "<leader>pf", require('telescope.builtin').find_files)
-      vim.keymap.set("n", "<leader>pa", require('telescope.builtin').help_tags)
+      require('telescope').setup {}
+      local builtin = require('telescope.builtin')
+      vim.keymap.set("n", "<leader>pf", builtin.find_files)
+      vim.keymap.set("n", "<leader>pp", builtin.git_files)
+      vim.keymap.set("n", "<leader>vh", builtin.help_tags)
+      vim.keymap.set("n", "<leader>ps", function()
+        builtin.grep_string({
+          search = vim.fn.input("Grep > ")
+        })
+      end)
       vim.keymap.set("n", "<leader>pn", function()
-        require('telescope.builtin').find_files {
+        builtin.find_files {
           cwd = vim.fn.stdpath("config")
         }
       end)
